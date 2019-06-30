e0a8ecfa98d4f5ac4902e891e5dddf0075546eb6
John Pham <johnphammail@gmail.com>
2019-06-29 20:11:14 -0700

    Add styling for details panel

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -6,13 +6,12 @@ export default class App extends Component {
 
   state = {
     allPokemon: [],
-    selectedPokemon: {}
+    selectedPokemon: undefined
   };
 
   async componentDidMount() {
     const data = await fetch(this.LIST_API);
     const json = await data.json();
-    console.log(json["results"]);
 
     this.setState({ allPokemon: json["results"] });
   }
@@ -39,14 +38,63 @@ export default class App extends Component {
     return `${SHINY_SPRITE_URL}/${id}.png`;
   }
 
+  async getPokemonDetail(url) {
+    const data = await fetch(url);
+    const json = await data.json();
+
+    this.setState({ selectedPokemon: json });
+  }
+
   render() {
+    const { allPokemon, selectedPokemon } = this.state;
+
     return (
       <main id="pokedex-container">
         <h1>PokeDex</h1>
 
+        <section id="pokemon-detail-container">
+          {selectedPokemon && (
+            <div id="pokemon-detail">
+              <hr />
+
+              <h2 className="pokemon-name">{selectedPokemon.name}</h2>
+
+              <table className="stats-table">
+                <tbody>
+                  <tr>
+                    <td>Height</td>
+                    <td>{selectedPokemon.height}</td>
+                  </tr>
+                  <tr>
+                    <td>Weight</td>
+                    <td>{selectedPokemon.weight}</td>
+                  </tr>
+                  <tr>
+                    <td>Types</td>
+                    <td>
+                      {selectedPokemon.types.map(type => (
+                        <span key={type.type.name} className="type">
+                          {" "}
+                          {type.type.name}
+                        </span>
+                      ))}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+
+              <hr />
+            </div>
+          )}
+        </section>
+
         <section id="pokemon-list">
-          {this.state.allPokemon.map(pokemon => (
-            <div key={pokemon.name} className="pokemon-entry">
+          {allPokemon.map(pokemon => (
+            <div
+              key={pokemon.name}
+              className="pokemon-entry"
+              onClick={() => this.getPokemonDetail(pokemon.url)}
+            >
               <img
                 src={`${this.getNormalSprite(pokemon.url)}`}
                 alt={pokemon.name}
